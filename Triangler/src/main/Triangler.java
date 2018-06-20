fa6f7c6ff9372855fffb88c8ad7151ab8062f5f3
FIC <FIC@LAB11-WS05.info.local>
2018-06-19 21:47:18 -0300

    Projeto Triangler

---
+++ Triangler/src/main/Triangler.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Triangler {
+
+	public static void main(String[] args) {
+	/*	String letraA = "A";
+		
+		for(int i = 0; i<4; i++) {
+			System.out.println(letraA);
+			letraA += "A";
+			
+		}*/
+		
+		
+	/* String letraA = "A";
+	 int cont = 0;
+	 while(cont <4) 
+	 for (int i = 0; i<4 ; i++) {
+		 System.out.println(letraA);
+		 letraA+= "A";
+	   cont++;
+	  }
+	 }*/
+	}
+	}
+
+
